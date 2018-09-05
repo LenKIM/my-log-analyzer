41300a244bbbca72431e79b4c98018b5fbb49e37
len <joenggyu0@gmail.com>
2018-09-05 09:08:45 +0900

    fix: make freeze contants

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,19 +1,58 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python3
-import Const
 
-Const.INDEX_OF_INTERNET_ID = 0
-Const.INDEX_OF_THREAD_NAME = 1
-Const.INDEX_OF_IDENTD = 2
-Const.INDEX_OF_USER_ID = 3
-Const.INDEX_OF_DATETIME_IN_LOG = 4
-Const.INDEX_OF_REST_METHOD = 5
-Const.INDEX_OF_REST_API = 6
-Const.INDEX_OF_HTTP_VERSION = 7
-Const.INDEX_OF_STATUS_NUMBER = 8
-Const.INDEX_OF_PACKET_SIZE = 9
-Const.INDEX_OF_BASE_URL = 10
-Const.INDEX_OF_USER_AGENT = 11
-Const.INDEX_OF_USER_AGENT_IF_NOT_EXIST = 12
-Const.INDEX_OF_RESPONSE_TIME = 13
 
+def INDEX_OF_INTERNET_ID():
+    return 0
+
+
+def INDEX_OF_THREAD_NAME():
+    return 1
+
+
+def INDEX_OF_IDENTD():
+    return 2
+
+
+def INDEX_OF_USER_ID():
+    return 3
+
+
+def INDEX_OF_DATETIME_IN_LOG():
+    return 4
+
+
+def INDEX_OF_REST_METHOD():
+    return 5
+
+
+def INDEX_OF_REST_API():
+    return 6
+
+
+def INDEX_OF_HTTP_VERSION():
+    return 7
+
+
+def INDEX_OF_STATUS_NUMBER():
+    return 8
+
+
+def INDEX_OF_PACKET_SIZE():
+    return 9
+
+
+def INDEX_OF_BASE_URL():
+    return 10
+
+
+def INDEX_OF_USER_AGENT():
+    return 11
+
+
+def INDEX_OF_USER_AGENT_IF_NOT_EXIST():
+    return 12
+
+
+def INDEX_OF_RESPONSE_TIME():
+    return 13
