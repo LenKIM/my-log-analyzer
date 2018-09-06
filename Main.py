8e553bb9f908ab357be6b928812d9966177b7f0b
len <joenggyu0@gmail.com>
2018-09-07 08:33:58 +0900

    fix: main 함수 수정

--- Main.py
+++ Main.py
@@ -12,9 +12,10 @@ from function.function_03 import Function03Impl
 from function.function_04 import Function04Impl
 from function.function_05 import Function05Impl
 from helpers.command_line_helper import CommandLineHelper
-from helpers.constants import FUNCTION_01, FUNCTION_02, FUNCTION_03, FUNCTION_04, FUNCTION_05, FUNCTION_06
+from constants import FUNCTION_01, FUNCTION_02, FUNCTION_03, FUNCTION_04, FUNCTION_05, FUNCTION_06
 from helpers.datetime_control_helper import TimeControlHelper
 from helpers.file_reader_helper import FileReaderHelper
+from helpers.log_parser_helper import LogParserHelper
 
 if __name__ == '__main__':
     CommandLineHelper.intro()
@@ -32,6 +33,7 @@ if __name__ == '__main__':
         start_day = cus_time.start_time.day
         end_day = cus_time.end_time.day
         files_path_list = TimeControlHelper.select_days_from_datetimes(start_day, end_day)
+        # files_path_list = ['/Users/len/log-analyer-assignment/tests/fixtures/text.txt']
 
         file_reader_helper = FileReaderHelper()
 
@@ -40,11 +42,11 @@ if __name__ == '__main__':
             FileReaderHelper.read_logs_only_request_api_and_response_time,
             (files_path_list, cus_time.start_time, cus_time.end_time))
         end_count_time = time.time()
-        # the_longest_response_time_request_api = function_01.extract_the_longest_response_time_request_api(
-        #     log_lines_fr12om_start_time_to_end_time)
-        print(end_count_time - start_count_time)
-        # 24/Aug/2018:00:01:02 24/Aug/2018:05:01:30
         print(log_lines_from_start_time_to_end_time[0])
+        print(log_lines_from_start_time_to_end_time[1])
+        print(log_lines_from_start_time_to_end_time[2])
+
+        print(end_count_time - start_count_time)
 
     elif select_number is FUNCTION_02():
 
@@ -87,41 +89,46 @@ if __name__ == '__main__':
                           , ),
         ]
         answer = inquirer.prompt(http_method_question, theme=GreenPassion())
-        selected_http_method_from_question = answer.get('select_http_method')[0]
+
+        selected_http_method_from_question = answer.get('select_http_method')
 
         http_method = '-1'
-        if selected_http_method_from_question is '5':
+        if selected_http_method_from_question[0] is '5':
             http_method = input('그 외 버튼을 누르셨습니다. 원하시는 HTTP_METHOD 을 입력해주시기 바랍니다.')
         else:
             http_method = selected_http_method_from_question.split(' ')[1]
 
         start_day = cus_time.start_time.day
         end_day = cus_time.end_time.day
-
         files_path_list = TimeControlHelper.select_days_from_datetimes(start_day, end_day)
         log_lines_from_start_time_to_end_time = FileReaderHelper.read_logs_between_datetime(files_path_list,
                                                                                             cus_time.start_time,
                                                                                             cus_time.start_time)
+        file_reader_helper = FileReaderHelper()
+
         function_02 = Function02Impl()
+        result = {}
+        http_code = '200'
+        http_method = 'GET'
         for single_line in log_lines_from_start_time_to_end_time:
 
+            single_line = LogParserHelper.custom_log_parser(single_line)
+
             if function_02.is_satisfied_http_method(single_line,
-                                                    http_method) is True \
-                    and function_02.is_satisfied_http_status(single_line, http_code):
-                function_02.collect_all_satisfied_request_api(single_line)
+                                                    http_method) is True and function_02.is_satisfied_http_status(
+                single_line, http_code):
+                result = function_02.collect_all_satisfied_request_api(single_line)
 
-        sorted_function_02_lists = OrderedDict(sorted(function_02.result.items()))
-        print('현재 선택 하신 HTTP_CODE 는 ' + http_method + ', HTTP_METHOD 는 ' + http_method + '입니다.')
-
+        sorted_function_02_lists = OrderedDict(sorted(result.items()))
+        print('현재 선택 하신 HTTP_CODE 는 ' + http_code + ', HTTP_METHOD 는 ' + http_method + '입니다.')
         for single_data in sorted_function_02_lists:
             print("Request API {}: COUNT {}".format(single_data, sorted_function_02_lists[single_data]))
 
         print('정렬의 경우에는 호출된 API 건수에 따라 내림차순으로 출력되어집니다.')
+
     elif select_number is FUNCTION_03():
 
         function_03 = Function03Impl()
-
-        # TODO 시작시간과 종료시간 인터별 입력받고 파일 호출하는대서 인터벌 시간 간격으로 잘라서 프린트 하.
 
         range_times_list = cus_time.range_times
         start_day = cus_time.start_time.day
@@ -131,25 +138,37 @@ if __name__ == '__main__':
         status_result = function_03.count_http_status_code(files_path_list, range_times_list)
         print(status_result)
 
-        pass
     elif select_number is FUNCTION_04():
         # 정적 리소스(image, javascript, css)가 아닌 특정 시간에 가장 많이 호출된 API 를 Listing 함
 
         function_04 = Function04Impl()
         start_day = cus_time.start_time.day
-        end_day = cus_time.end_time
+        end_day = cus_time.end_time.day
         files_path_list = TimeControlHelper.select_days_from_datetimes(start_day, end_day)
 
+        file_reader_helper = FileReaderHelper()
+
+        log_lines_from_start_time_to_end_time = file_reader_helper.multiprocessing_process(
+            FileReaderHelper.read_logs_between_datetime,
+            (files_path_list, cus_time.start_time, cus_time.end_time))
         request_api_result = function_04.count_many_call_request_request_api(files_path_list)
         print(request_api_result)
 
     elif select_number is FUNCTION_05():
+
         function_05 = Function05Impl()
 
         start_day = cus_time.start_time.day
         end_day = cus_time.end_time
+
+        file_reader_helper = FileReaderHelper()
+
         files_path_list = TimeControlHelper.select_days_from_datetimes(start_day, end_day)
 
-    pass
+        log_lines_from_start_time_to_end_time = file_reader_helper.multiprocessing_process(
+            FileReaderHelper.read_logs_between_datetime,
+            (files_path_list, cus_time.start_time, cus_time.end_time))
+
+        function_05.abstract_client_agent_info_from_request_api(log_lines_from_start_time_to_end_time)
 
 print('계속해서 다른 기능을 사용하고 싶으시면\n 아무 키나 눌러주세요.')
