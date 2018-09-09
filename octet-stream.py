ed5999d2f8c216652a926122eba9d138d1c9b24f
len <joenggyu0@gmail.com>
2018-09-10 03:18:46 +0900

    chore: read test complete

--- octet-stream.py
+++ octet-stream.py
@@ -1,10 +1,15 @@
 import glob
 import multiprocessing
 import os
+import time
+from typing import List
 
 from tqdm import tqdm
 
-file_paths = glob.glob("/Users/len/log-analyer-assignment/20180824/*.txt")
+from function.function_01 import Function01Impl
+from helpers.log_parser_helper import LogParserHelper
+
+file_paths = glob.glob("/Users/len/log-analyer-assignment/logdata/20180824/*.txt")
 
 
 def get_filename_with_ext(filepath):
@@ -20,29 +25,36 @@ def get_filename(filepath):
 def read_log_files(file_paths):
     jobs = []
     manager = multiprocessing.Manager()
-    return_dict = manager.dict()
+    return_list = manager.list()
+    # return_dict = manager.dict()
     for file_path in file_paths:
-        p = multiprocessing.Process(target=read_log_worker, args=(file_path, return_dict))
+        p = multiprocessing.Process(target=read_log_worker, args=(file_path, return_list))
         jobs.append(p)
         p.start()
 
     for proc in jobs:
         proc.join()
 
-    return return_dict
+    return return_list
 
 
-def read_log_worker(file_path, return_dict):
+def read_log_worker(file_path, return_list) -> List:
     f = open(file_path, 'r', encoding='utf8')
     lines = f.readlines()
     line_count = 0
     for line in tqdm(lines):
+        a = LogParserHelper.custom_log_parser(line)
         line_count = line_count + 1
-
-    return_dict[get_filename(file_path)] = line_count
+        return_list.append(a)
     f.close()
+    return return_list
 
 
-dic = read_log_files(file_paths)
-for key in dic.keys():
-    print(key, ":", dic[key])
+s = time.time()
+function_01 = Function01Impl()
+list = read_log_files(file_paths)
+print(function_01.extract_the_longest_response_time_request_api(list))
+e = time.time()
+print(e - s)
+# for key in dic.keys():
+#     print(key, ":", dic[key]) 
