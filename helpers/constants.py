0885fbf1f51cf687e8daccff059e5b9937be44d1
len <joenggyu0@gmail.com>
2018-09-13 08:47:04 +0900

    chore: 상수 파일을 helpers 디렉토리로 이동

---
+++ helpers/constants.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+
+def INDEX_OF_INTERNET_ID():
+    return 0
+
+
+def INDEX_OF_THREAD_NAME():
+    return 1
+
+
+def INDEX_OF_IDENTD():
+    return 2
+
+
+def INDEX_OF_USER_ID():
+    return 3
+
+
+def INDEX_OF_DATETIME_IN_LOG():
+    return 4
+
+
+def INDEX_OF_REST_METHOD():
+    return 5
+
+
+def INDEX_OF_REST_API():
+    return 6
+
+
+def INDEX_OF_HTTP_VERSION():
+    return 7
+
+
+def INDEX_OF_STATUS_NUMBER():
+    return 8
+
+
+def INDEX_OF_PACKET_SIZE():
+    return 9
+
+
+def INDEX_OF_BASE_URL():
+    return 10
+
+
+def INDEX_OF_USER_AGENT():
+    return 11
+
+
+def INDEX_OF_USER_AGENT_IF_NOT_EXIST():
+    return 12
+
+
+def INDEX_OF_RESPONSE_TIME():
+    return 13
+
+
+def FUNCTION_01():
+    return 1
+
+
+def FUNCTION_02():
+    return 2
+
+
+def FUNCTION_03():
+    return 3
+
+
+def FUNCTION_04():
+    return 4
+
+
+def FUNCTION_05():
+    return 5
+
+
+def FUNCTION_06():
+    return 6
